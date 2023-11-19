dd4a2069279d7812a008e8126de54acddfff2d10
Shlongoh <77902863+Shlongoh@users.noreply.github.com>
2023-11-18 19:19:19 -0500

    Update UpdateScript.py

--- UpdateScript.py
+++ UpdateScript.py
@@ -19,12 +19,44 @@ def clone_repositories(repo_urls, destination_folder='.'):
 if __name__ == "__main__":
     # Example array of Git repository URLs
     red_urls = [
-        'https://github.com/SpecterOps/BloodHound.git'
+        'https://github.com/SpecterOps/BloodHound.git',
+        'https://github.com/BishopFox/sliver.git',
+        'https://github.com/fortra/impacket.git',
+        'https://github.com/redcanaryco/atomic-red-team.git',
+        'https://github.com/carlospolop/PEASS-ng.git',
+        'https://github.com/DominicBreuker/pspy.git',
+        'https://github.com/1N3/Sn1per.git',
+        'https://github.com/Tib3rius/AutoRecon.git',
+        'https://github.com/danielmiessler/SecLists.git',
+        'https://github.com/matthewdunwoody/POSHSPY.git',
+        'https://github.com/mschwager/fierce.git',
+        'https://github.com/ParrotSec/shellter.git',
+        'https://github.com/iphelix/dnschef.git',
+        'https://github.com/Veil-Framework/Veil.git',
+        'https://github.com/xxgrunge/sqlninja.git',
+        'https://github.com/jpillora/chisel.git',
+        'https://github.com/GhostPack/Certify.git',
+        'https://github.com/GhostPack/Rubeus.git',
+        'https://github.com/GhostPack/Seatbelt.git',
+        'https://github.com/GhostPack/SafetyKatz.git',
+        'https://github.com/JlSakuya/Linux-Privilege-Escalation-Exploits.git',
+        'https://github.com/BeichenDream/GodPotato.git',
+        'https://github.com/ParrotSec/mimikatz.git',
+        'https://github.com/itm4n/FullPowers.git',
+        'https://github.com/GTFOBins/GTFOBins.github.io.git',
+        'https://github.com/LOLBAS-Project/LOLBAS.git',
+        'https://github.com/WADComs/WADComs.github.io.git',
+        '
         # Add more repository URLs as needed
     ]
   
     blue_urls = [
-        'https://github.com/mandiant/flare-floss.git'
+        'https://github.com/mandiant/flare-floss.git',
+        'https://github.com/rizinorg/cutter.git',
+        'https://github.com/sleuthkit/autopsy.git',
+        'https://github.com/VirusTotal/yara.git',
+        'https://github.com/magicsword-io/LOLDrivers.git',
+        '
         # Add more repository URLs as needed
     ]
 
